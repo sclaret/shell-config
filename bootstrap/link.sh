4b46fae485ec1a1b7f79680680d50654ad8881d8
Simon Claret <sclaret@gmail.com>
2021-05-27 12:39:35 -0400

    improve dotfiles bootstrap
    bootstrap
    
    bootstrap

--- bootstrap/link.sh
+++ bootstrap/link.sh
@@ -1,25 +1,31 @@
 #! /usr/bin/env bash
 
-#set -o xtrace
+set +x
 
 function link_config {
-  echo -n "Linking $2... "
+  echo "Need a symbolic link pointing to"
+  echo "source_file $1"
+  echo "from link_name $2" 
+  set -x
   if [[ -h $2 ]]; then
-    echo "link exists, skipping"
-    return
+    echo "$2 is a symbolic link"
+    rm $2
   elif [[ -f $2 ]]; then
-    echo "file exists, backing up"
-    echo "NOT IMPLEMENTED"
-    exit
+    tput smso
+    echo "$2 is a file"
+    mv $2 $2$(date +%s)
+    tput rmso
   elif [[ -d $2 ]]; then
-    echo "directory exists, backing up"
-    echo "NOT IMPLEMENTED"
-    exit
+    tput smso
+    echo "$2 is a directory"
+    mv $2 $2$(date +%s)
+    tput rmso
   else
-    echo "succeeded"
+    echo "$2 does not exist"
   fi
-  mkdir -p $(dirname $2)
-  (set -x; ln -s $1 $2)
+  ln -vs $1 $2
+  set +x
+  echo
 }
 
 link_config $PWD/.config ~/.config
@@ -27,3 +33,10 @@ link_config $PWD/.config ~/.config
 link_config $PWD/.bash_profile ~/.bash_profile
 link_config $PWD/.bashrc ~/.bashrc
 link_config $PWD/.bash_aliases ~/.bash_aliases
+
+
+set -x
+# Check config files working
+git config user.name
+git config user.email
+set +x
